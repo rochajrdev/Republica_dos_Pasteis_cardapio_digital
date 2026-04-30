78f59a30de2c91f02c879a4b4205f4bfff928e48
Adailson Rocha <juniorbing0317@gmail.com>
2026-04-30 11:42:43 -0300

    Feat: Nova logo principal

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -4,10 +4,10 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider 
-      attribute="class" 
-      defaultTheme="system" 
-      enableSystem 
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
       disableTransitionOnChange
     >
       {children}
